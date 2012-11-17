75d2c1acd73241d948e0ef8d446cb9ec0e40827a
Markus Klemm <markus@markusklemm.net>
2012-11-17 16:40:01 +0100

    Jetzt schnell dank Kongruenzschreibweise

--- Mersenne.py
+++ Mersenne.py
@@ -6,19 +6,18 @@
 
 def PrimMersenne(p):
     """Berechnet die Mersennezahl der Ordnung gegebenen Ordnung p und gibt diese 
-    aus, wenn diese eine Primzahl ist. Benutzt hierfür die LucasLehmerFolge 
-    Funktion."""
+    aus, wenn diese eine Primzahl ist. Ansonsten none"""
     m = 2**p - 1 #=Mersennezahl
     if (p==2):
         return m
     
-    #LucasLehmerFolge
+    #LucasLehmerFolge Beginn
     s = [0,4]
-    for x in (range(2,p)):
-        s.append(s[x-1]**2 - 2)
     
-    #LucasLehmerTest
-    if (s[p-1] % m == 0):
+    for x in (range(2,p)):
+        s.append((s[x-1]**2 - 2) % m) #Primzahlentest mithilfe LucasLahmer Test
+        #und Kongruenz
+        if (s[x] == 0):
             return m
         
     return
@@ -27,5 +26,5 @@ def PrimMersenne(p):
 for x in (range(1,25)):
     print PrimMersenne(x)
     
-print "Große Zahl"    
+print "Große Zahl"
 print PrimMersenne(11213)
